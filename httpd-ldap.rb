561a6c0406ba544a6728a249f21a4e94f4e75c28
axelbrunger <58152417+axelbrunger@users.noreply.github.com>
2022-02-07 10:35:59 -0800

    httpd with openldap support
    Homebrew Formulae for httpd and apr-util with openldap support.
    
    Note: depends on openldap

---
+++ httpd-ldap.rb
@@ -0,0 +1,181 @@
+class Httpd < Formula
+  desc "Apache HTTP server"
+  homepage "https://httpd.apache.org/"
+  url "https://dlcdn.apache.org/httpd/httpd-2.4.52.tar.bz2"
+  mirror "https://downloads.apache.org/httpd/httpd-2.4.52.tar.bz2"
+  sha256 "0127f7dc497e9983e9c51474bed75e45607f2f870a7675a86dc90af6d572f5c9"
+  license "Apache-2.0"
+
+  bottle do
+    sha256 arm64_monterey: "7c71a42a41693093015c11813cd3ec9e714f303884afde9bde61b3eb86f2fbae"
+    sha256 arm64_big_sur:  "2bda2c7798b135b519882bd010bc2b4c8b6fb243c69046713639b22cff689400"
+    sha256 monterey:       "6a35059e2c3f69635ac6035c0a638030a1038cc62e648604ab53b4a280a88ba8"
+    sha256 big_sur:        "59ee7e0491e7a79a8844b05d0f4075d9b76b707190e6f229b84f763b58b9728a"
+    sha256 catalina:       "af1a311706c4eb1adb63b2179e2677e305f49ec346e74c3a29e60fe9fa8e5d60"
+    sha256 x86_64_linux:   "6d602ea4372d756c6ffb622373c5daa750ddb79d65818e4fa686070025b7f8ae"
+  end
+
+  depends_on "apr"
+  depends_on "apr-util"
+  depends_on "brotli"
+  depends_on "libnghttp2"
+  depends_on "openssl@1.1"
+  depends_on "pcre"
+depends_on "openldap"
+
+  uses_from_macos "libxml2"
+  uses_from_macos "zlib"
+
+  def install
+    # fixup prefix references in favour of opt_prefix references
+    inreplace "Makefile.in",
+      '#@@ServerRoot@@#$(prefix)#', '#@@ServerRoot@@'"##{opt_prefix}#"
+    inreplace "docs/conf/extra/httpd-autoindex.conf.in",
+      "@exp_iconsdir@", "#{opt_pkgshare}/icons"
+    inreplace "docs/conf/extra/httpd-multilang-errordoc.conf.in",
+      "@exp_errordir@", "#{opt_pkgshare}/error"
+
+    # fix default user/group when running as root
+    inreplace "docs/conf/httpd.conf.in", /(User|Group) daemon/, "\\1 _www"
+
+    # use Slackware-FHS layout as it's closest to what we want.
+    # these values cannot be passed directly to configure, unfortunately.
+    inreplace "config.layout" do |s|
+      s.gsub! "${datadir}/htdocs", "${datadir}"
+      s.gsub! "${htdocsdir}/manual", "#{pkgshare}/manual"
+      s.gsub! "${datadir}/error",   "#{pkgshare}/error"
+      s.gsub! "${datadir}/icons",   "#{pkgshare}/icons"
+    end
+
+    libxml2 = "#{MacOS.sdk_path_if_needed}/usr"
+    libxml2 = Formula["libxml2"].opt_prefix if OS.linux?
+    zlib = if OS.mac?
+      "#{MacOS.sdk_path_if_needed}/usr"
+    else
+      Formula["zlib"].opt_prefix
+    end
+    system "./configure", "--enable-layout=Slackware-FHS",
+                          "--prefix=#{prefix}",
+                          "--sbindir=#{bin}",
+                          "--mandir=#{man}",
+                          "--sysconfdir=#{etc}/httpd",
+                          "--datadir=#{var}/www",
+                          "--localstatedir=#{var}",
+                          "--enable-mpms-shared=all",
+                          "--enable-mods-shared=all",
+                          "--enable-authnz-fcgi",
+                          "--enable-cgi",
+                          "--enable-pie",
+                          "--enable-suexec",
+"--enable-ldap",
+                          "--with-suexec-bin=#{opt_bin}/suexec",
+                          "--with-suexec-caller=_www",
+                          "--with-port=8080",
+                          "--with-sslport=8443",
+                          "--with-apr=#{Formula["apr"].opt_prefix}",
+                          "--with-apr-util=#{Formula["apr-util"].opt_prefix}",
+                          "--with-brotli=#{Formula["brotli"].opt_prefix}",
+                          "--with-libxml2=#{libxml2}",
+                          "--with-mpm=prefork",
+                          "--with-nghttp2=#{Formula["libnghttp2"].opt_prefix}",
+                          "--with-ssl=#{Formula["openssl@1.1"].opt_prefix}",
+                          "--with-pcre=#{Formula["pcre"].opt_prefix}",
+                          "--with-z=#{zlib}",
+                          "--disable-lua",
+                          "--disable-luajit",
+"--with-ldap"
+    system "make"
+    ENV.deparallelize if OS.linux?
+    system "make", "install"
+
+    # suexec does not install without root
+    bin.install "support/suexec"
+
+    # remove non-executable files in bin dir (for brew audit)
+    rm bin/"envvars"
+    rm bin/"envvars-std"
+
+    # avoid using Cellar paths
+    inreplace %W[
+      #{include}/httpd/ap_config_layout.h
+      #{lib}/httpd/build/config_vars.mk
+    ] do |s|
+      s.gsub! lib/"httpd/modules", HOMEBREW_PREFIX/"lib/httpd/modules"
+    end
+
+    inreplace %W[
+      #{bin}/apachectl
+      #{bin}/apxs
+      #{include}/httpd/ap_config_auto.h
+      #{include}/httpd/ap_config_layout.h
+      #{lib}/httpd/build/config_vars.mk
+      #{lib}/httpd/build/config.nice
+    ] do |s|
+      s.gsub! prefix, opt_prefix
+    end
+
+    inreplace "#{lib}/httpd/build/config_vars.mk" do |s|
+      pcre = Formula["pcre"]
+      s.gsub! pcre.prefix.realpath, pcre.opt_prefix
+      s.gsub! "${prefix}/lib/httpd/modules", HOMEBREW_PREFIX/"lib/httpd/modules"
+      s.gsub! Superenv.shims_path, HOMEBREW_PREFIX/"bin"
+    end
+  end
+
+  def post_install
+    (var/"cache/httpd").mkpath
+    (var/"www").mkpath
+  end
+
+  def caveats
+    <<~EOS
+      DocumentRoot is #{var}/www.
+
+      The default ports have been set in #{etc}/httpd/httpd.conf to 8080 and in
+      #{etc}/httpd/extra/httpd-ssl.conf to 8443 so that httpd can run without sudo.
+    EOS
+  end
+
+  plist_options manual: "apachectl start"
+
+  service do
+    run [opt_bin/"httpd", "-D", "FOREGROUND"]
+    environment_variables PATH: std_service_path_env
+    run_type :immediate
+  end
+
+  test do
+    # Ensure modules depending on zlib and xml2 have been compiled
+    assert_predicate lib/"httpd/modules/mod_deflate.so", :exist?
+    assert_predicate lib/"httpd/modules/mod_proxy_html.so", :exist?
+    assert_predicate lib/"httpd/modules/mod_xml2enc.so", :exist?
+
+    begin
+      port = free_port
+
+      expected_output = "Hello world!"
+      (testpath/"index.html").write expected_output
+      (testpath/"httpd.conf").write <<~EOS
+        Listen #{port}
+        ServerName localhost:#{port}
+        DocumentRoot "#{testpath}"
+        ErrorLog "#{testpath}/httpd-error.log"
+        PidFile "#{testpath}/httpd.pid"
+        LoadModule authz_core_module #{lib}/httpd/modules/mod_authz_core.so
+        LoadModule unixd_module #{lib}/httpd/modules/mod_unixd.so
+        LoadModule dir_module #{lib}/httpd/modules/mod_dir.so
+        LoadModule mpm_prefork_module #{lib}/httpd/modules/mod_mpm_prefork.so
+      EOS
+
+      pid = fork do
+        exec bin/"httpd", "-X", "-f", "#{testpath}/httpd.conf"
+      end
+      sleep 3
+
+      assert_match expected_output, shell_output("curl -s 127.0.0.1:#{port}")
+    ensure
+      Process.kill("TERM", pid)
+      Process.wait(pid)
+    end
+  end
+end
